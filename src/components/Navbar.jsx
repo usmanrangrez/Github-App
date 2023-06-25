762a4353030f6cacee1d2ef8ca659e5233f596ad
usmanrangrez <usmaanrangrez@gmail.com>
2023-06-25 18:00:24 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,23 @@
 import React from "react";
-import { Box, Flex, Image, Button } from "@chakra-ui/react";
+import { Box, Flex, Image } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <>
-      <Flex justify={"space-between"} align={"center"} py={2}>
-        <Box position={"relative"} ratio={5 / 3} minHeight={20}>
-          <Image
-            borderRadius="full"
-            boxSize="150px"
-            objectFit="contain"
-            src="../public/logo.png"
-            fill
-            alt="Github Logo"
-            sx={{
-              filter: "invert(1)",
-            }}
-          />
-        </Box>
-        <Box></Box>
-      </Flex>
-    </>
+    <Flex justify="space-between" align="center" py={2}>
+      <Box flex="1">
+        <Image
+          borderRadius="full"
+          boxSize={["100px", "150px"]}
+          objectFit="contain"
+          src="/logo.png"
+          alt="Github Logo"
+          sx={{
+            filter: "invert(1)",
+          }}
+        />
+      </Box>
+      <Box flex="1"></Box>
+    </Flex>
   );
 };
 
