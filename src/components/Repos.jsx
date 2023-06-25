929e97372c59889c0cc6802aeb96ab5698b25877
usmanrangrez <usmaanrangrez@gmail.com>
2023-06-25 17:59:47 +0530

    Update Repos.jsx

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -9,6 +9,7 @@ import {
   Box,
   Stack,
   Button,
+  useBreakpointValue,
 } from "@chakra-ui/react";
 import axios from "axios";
 
@@ -18,7 +19,6 @@ const Repos = ({ reposUrl }) => {
   const [loading, setLoading] = useState(false);
   const [showMore, setShowMore] = useState(false);
 
-  console.log("respppsss", repos);
   useEffect(() => {
     const fetchRepos = async () => {
       if (!reposUrl) return;
@@ -44,115 +44,124 @@ const Repos = ({ reposUrl }) => {
     fetchRepos();
   }, [reposUrl, toast]);
 
+  const isMobileScreen = useBreakpointValue({ base: true, md: false });
+
   return (
     <>
       <Text
-        textAlign={"center"}
+        textAlign="center"
         letterSpacing={1.5}
-        fontSize={"3xl"}
-        fontWeight={"bold"}
-        color={"green.400"}
+        fontSize={["2xl", "3xl"]}
+        fontWeight="bold"
+        color="green.400"
         mt={4}
       >
         REPOSITORIES
       </Text>
       {loading && (
-        <Flex justifyContent={"center"}>
-          <Spinner size={"xl"} my={4} />
+        <Flex justifyContent="center">
+          <Spinner size="xl" my={4} />
         </Flex>
       )}
       {repos
         .sort((a, b) => b.stargazers_count - a.stargazers_count)
-        .map((rep, idx) => {
-          if (idx > 4 && !showMore) return null; //pehle 0-4 mai ye nai chalega
+        .map((repo, idx) => {
+          if (idx > 4 && !showMore) return null;
           return (
             <Flex
-              key={rep.id}
+              key={repo.id}
               padding={4}
-              bg={"green.800"}
+              bg="green.800"
               _hover={{ bg: "whiteAlpha.400" }}
               my={4}
               px={10}
               gap={4}
               borderRadius={4}
-              transition={"all 0.3 ease"}
-              justifyContent={"space-between"}
-              alignItems={"center"}
+              transition="all 0.3s ease"
+              justifyContent="space-between"
+              alignItems="center"
               mt={2}
               mr={10}
               ml={10}
             >
-              <Flex flex={1} direction={"column"}>
+              <Flex flex={1} direction="column">
                 <Link
-                  href={rep.html_url}
+                  href={repo.html_url}
                   target="_blank"
-                  fontSize={"md"}
-                  fontWeight={"bold"}
+                  fontSize="md"
+                  fontWeight="bold"
+                  style={{ wordBreak: "break-all" }}
                 >
-                  {rep.name}
+                  {repo.name}
                 </Link>
                 <Badge
-                  fontSize={"0.7em"}
-                  colorScheme={"whatsapp"}
-                  textAlign={"center"}
+                  fontSize={["0.6em", "0.7em"]}
+                  colorScheme="whatsapp"
+                  textAlign="center"
                   px={1}
                   mt={1}
-                  width={"min-content"}
+                  width="min-content"
                 >
-                  LANGUAGE: {rep.language ? rep.language : " None"}
+                  LANGUAGE: {repo.language ? repo.language : " None"}
                 </Badge>
               </Flex>
-              <Stack direction={"row"}>
-                <Badge
-                  bgColor={"yellow.500"}
-                  color={"black"}
-                  p={2}
-                  borderRadius={10}
-                  marginRight={4}
-                >
-                  STARS: {rep.stargazers_count ? rep.stargazers_count : 0}
-                </Badge>
-                <Badge
-                  bgColor={"pink"}
-                  color={"black"}
-                  p={2}
-                  borderRadius={10}
-                  marginRight={4}
-                >
-                  FORKS: {rep.forks ? rep.forks : 0}
-                </Badge>
-                <Badge
-                  bgColor={"blue.300"}
-                  color={"black"}
-                  p={2}
-                  borderRadius={10}
-                  marginRight={4}
-                >
-                  WATCHERS: {rep.watchers ? rep.watchers : 0}
-                </Badge>
-              </Stack>
+
+              {!isMobileScreen && (
+                <Stack direction="row">
+                  <Badge
+                    bgColor="yellow.500"
+                    color="black"
+                    p={2}
+                    borderRadius={10}
+                    marginRight={4}
+                    fontSize={["0.8em", "1em"]}
+                  >
+                    STARS: {repo.stargazers_count ? repo.stargazers_count : 0}
+                  </Badge>
+                  <Badge
+                    bgColor="pink"
+                    color="black"
+                    p={2}
+                    borderRadius={10}
+                    marginRight={4}
+                    fontSize={["0.8em", "1em"]}
+                  >
+                    FORKS: {repo.forks ? repo.forks : 0}
+                  </Badge>
+                  <Badge
+                    bgColor="blue.300"
+                    color="black"
+                    p={2}
+                    borderRadius={10}
+                    marginRight={4}
+                    fontSize={["0.8em", "1em"]}
+                  >
+                    WATCHERS: {repo.watchers ? repo.watchers : 0}
+                  </Badge>
+                </Stack>
+              )}
             </Flex>
           );
         })}
-
-      {/* //map huwa 0-4 fir ye run hoga... AGAR  repos ki len originally 5 se zyada hai tohi showmoire button dikahu  */}
-      {/* AGAR SHOWMORE TRUE HAI ye isilye kyuki fir 2 buttons ayege */}
       {!showMore && repos.length > 5 && (
-        <Flex justifyContent={"center"} my={4}>
+        <Flex justifyContent="center">
           <Button
-            size={"md"}
-            colorScheme="whatsapp"
+            colorScheme="green"
+            size="md"
+            m={4}
+            w={"min-content"}
             onClick={() => setShowMore(true)}
           >
             Show More
           </Button>
         </Flex>
       )}
-      {showMore && (
-        <Flex justifyContent={"center"} my={4}>
+      {showMore && repos.length > 5 && (
+        <Flex justifyContent="center">
           <Button
-            size={"md"}
-            colorScheme="whatsapp"
+            colorScheme="green"
+            size="md"
+            m={4}
             onClick={() => setShowMore(false)}
           >
             Show Less
