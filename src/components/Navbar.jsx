303d5fbaa45227e0b3751e08fe2d57c273e097ec
usmanrangrez <usmaanrangrez@gmail.com>
2023-07-05 16:19:53 +0530

    my changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import { Box, Button, Flex, Image, useDisclosure } from "@chakra-ui/react";
 import HistoryModal from "./HistoryModal";
 
-import { Box, Flex, Image } from "@chakra-ui/react";
-
 const Navbar = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
@@ -28,8 +26,6 @@ const Navbar = () => {
         </Button>
       </Box>
       {isOpen && <HistoryModal isOpen={isOpen} onClose={onClose} />}
-
-      <Box flex="1"></Box>
     </Flex>
   );
 };
